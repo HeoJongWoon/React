06497926afb6358d6204c7242dacf001443d3ca9
HeoJongWoon <whddns0902@gmail.com>
2023-10-23 11:00:11 +0900

    커스텀훅으로 전역 클리이언트 데이터 관리

--- src/components/common/modal/Modal.jsx
+++ src/components/common/modal/Modal.jsx
@@ -1,30 +1,20 @@
 import './Modal.scss';
 import { useEffect } from 'react';
-import { useDispatch, useSelector } from 'react-redux';
-import { close } from '../../../redux/modalSlice';
 import { AnimatePresence, motion } from 'framer-motion';
+import { useGlobalData } from '../../../hooks/useGlobalContext';
 
 const Modal = ({ children }) => {
-	const dispatch = useDispatch();
-	const { isOpen } = useSelector((store) => store.modal);
+	const { ModalOpen, setModalOpen } = useGlobalData();
 
 	useEffect(() => {
-		isOpen
-			? (document.body.style.overflow = 'hidden')
-			: (document.body.style.overflow = 'auto');
-	}, [isOpen]);
+		ModalOpen ? (document.body.style.overflow = 'hidden') : (document.body.style.overflow = 'auto');
+	}, [ModalOpen]);
 	return (
 		<AnimatePresence>
-			{isOpen && (
-				<motion.aside
-					className='modal'
-					initial={{ opacity: 0 }}
-					animate={{ opacity: 1 }}
-					exit={{ opacity: 0 }}
-					transition={{ duration: 0.5 }}
-				>
+			{ModalOpen && (
+				<motion.aside className='modal' initial={{ opacity: 0 }} animate={{ opacity: 1 }} exit={{ opacity: 0 }} transition={{ duration: 0.5 }}>
 					<div className='con'>{children}</div>
-					<span onClick={() => dispatch(close())}>close</span>
+					<span onClick={() => setModalOpen(false())}>close</span>
 				</motion.aside>
 			)}
 		</AnimatePresence>
--- src/components/main/info/Info.jsx
+++ src/components/main/info/Info.jsx
@@ -4,7 +4,7 @@ import './Info.scss';
 function Info() {
 	const { data, isSuccess } = useFlickrQuery({
 		type: 'user',
-		id: '164021883@N04',
+		id: '199283169@N04',
 	});
 	return (
 		<section className='info myScroll'>
@@ -14,10 +14,7 @@ function Info() {
 						if (idx >= 6) return null;
 						return (
 							<article key={idx}>
-								<img
-									src={`https://live.staticflickr.com/${pic.server}/${pic.id}_${pic.secret}_b.jpg`}
-									alt={pic.title}
-								/>
+								<img src={`https://live.staticflickr.com/${pic.server}/${pic.id}_${pic.secret}_b.jpg`} alt={pic.title} />
 							</article>
 						);
 					})}
