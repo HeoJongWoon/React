7e3f0b7618cdcc13985fe248930ffbd14484d20f
HeoJongWoon <whddns0902@gmail.com>
2023-10-23 09:39:19 +0900

    유튜브의 디테일 컴포넌트 메모이제이션 처리 완료

--- src/components/sub/youtube/Detail.jsx
+++ src/components/sub/youtube/Detail.jsx
@@ -16,17 +16,14 @@ function Detail() {
 				console.log(json.items[0].snippet);
 				setData(json.items[0].snippet);
 			});
-	}, []);
+	}, [id]);
 
 	return (
 		<Layout title={'Detail'}>
 			<h2>{Data?.title}</h2>
 			<p>{Data?.description}</p>
 			<div className='vidBox'>
-				<iframe
-					src={`https://www.youtube.com/embed/${Data?.resourceId.videoId}`}
-					title='youtube'
-				></iframe>
+				<iframe src={`https://www.youtube.com/embed/${Data?.resourceId.videoId}`} title='youtube'></iframe>
 			</div>
 		</Layout>
 	);
